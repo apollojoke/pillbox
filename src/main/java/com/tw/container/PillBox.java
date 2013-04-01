3bc6c6a6585fbe4f9c25172206d8f3fed3d5a45c
Suo Qin <kpsesuo@gmail.com>
2013-04-01 14:13:00 +0800

    refactor, dynamic load class

--- src/main/java/com/tw/container/PillBox.java
+++ src/main/java/com/tw/container/PillBox.java
@@ -1,14 +1,32 @@
 package com.tw.container;
 
-import example.Aspirin;
+import org.yaml.snakeyaml.Yaml;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 public class PillBox {
-    public Object create_pill(String pillName) {
-        return new Aspirin();
+    private final Object target;
+
+    public PillBox(Object object) {
+        target = object;
+
     }
 
-    public static PillBox loadContext(String path) {
-        return new PillBox();
+    public Object create_pill(String pillName) {
+        return target;
+    }
+
+    public static PillBox loadContext(String path) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Yaml yaml = new Yaml();
+        String document = "- id: aspirin\n  class: example.Aspirin";
+        List<String> list = (List<String>) yaml.load(document);
+        System.out.println(list);
+        Class<?> clazz = Class.forName("example.Aspirin");
+        Constructor<?> ctor = clazz.getConstructor();
+        Object object = ctor.newInstance();
+        return new PillBox(object);
 
     }
 }
